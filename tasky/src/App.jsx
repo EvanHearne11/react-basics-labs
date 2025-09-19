a6cb803668e3877f06f03fc0eadd4f59f0ebffb4
Evan Hearne <20107542@mail.wit.ie>
2025-09-19 17:05:08 +0100

    added task state

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -1,36 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Task from './components/Task';
 import React, { useState } from 'react';
 
+
 function App() {
-  const [count, setCount] = useState(0)
-  
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" }
+      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { title: "Tidy up", deadline: "Today" }
     ]
   });
-
 
   return (
     <div className="container">
       <h1>Tasky</h1>
-     {tasksState.tasks.map((task) => (
-      <Task 
-    title ={task.title}
-    description={task.description}
-    deadline={task.deadline}
-    key={task.id}
-      />
-
-      ))}
+      <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description} />
+      <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description} />
+      <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description} />
     </div>
   );
 
 }
 
-export default App
+export default App;
