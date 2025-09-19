8f911ff4d310a59835c85bfa51c710e4807973ee
Evan Hearne <20107542@mail.wit.ie>
2025-09-19 16:22:10 +0100

    added keys to tasks

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -2,34 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", deadline: "Today" }
+    ]
+  });
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h1>Tasky</h1>
+     {tasksState.tasks.map((task) => (
+      <Task 
+    title ={task.title}
+    description={task.description}
+    deadline={task.deadline}
+    key={task.id}
+      />
+
+      ))}
+    </div>
+  );
+
 }
 
 export default App
